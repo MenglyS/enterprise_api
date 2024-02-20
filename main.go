e61f8abeae1716610b17a3d909f24a658354f507
menglysovan124 <135255375+menglysovan124@users.noreply.github.com>
2024-02-20 07:25:25 +0700

    fix cors

--- main.go
+++ main.go
@@ -26,9 +26,27 @@ func init() {
 	}
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 
 	route := gin.Default()
+
+	route.Use(CORSMiddleware())
 
 	routers.InitRoute(route)
 
